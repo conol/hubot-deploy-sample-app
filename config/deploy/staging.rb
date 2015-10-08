0929cbec3f4eb4cb65ed6b3717ec252a7fa7859f
h.ooki <h.ooki@conol.jp>
2015-10-08 19:19:23 +0900

    デプロイ時のユーザーを指定

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,7 @@ set :unicorn_rack_env, 'deployment'
 
 server '54.65.164.152',
   roles: %w{app db web},
+  user: 'ubuntu',
   ssh_options: {
     forward_agent: true,
     auth_methods: %w{publickey},
