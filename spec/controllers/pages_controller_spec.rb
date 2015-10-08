7220dbc51898ecb2872bca58e2314d03fcc5e375
h.ooki <h.ooki@conol.jp>
2015-10-08 14:20:28 +0900

    簡易的なテストを記述

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  describe 'GET index' do
+    it 'should return http ok' do
+      get :index
+      expect(response).to be_ok
+    end
+  end
 end
